27b0bf0784ee8ceb4f627637973cd269ab315506
iwannacat <iwannacat08@gmail.com>
2025-04-06 01:09:45 +0900

    mission: 홈 판넬에 인디케이터 추가

--- mission/Flo/app/src/main/java/com/example/flo/ui/home/HomeFragment.kt
+++ mission/Flo/app/src/main/java/com/example/flo/ui/home/HomeFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.replace
 import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager2.widget.ViewPager2
 import com.example.flo.MainActivity
@@ -24,28 +22,15 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        val homeViewModel = ViewModelProvider(this)[HomeViewModel::class.java]
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-        val panelAdapter = HomePanelVPAdapter(this)
-        panelAdapter.addItem(HomePanelFragment())
-        panelAdapter.addItem(HomePanelFragment())
-        binding.vpHomePanel.adapter = panelAdapter
-        //뷰페이져가 좌우로 스크롤 될 수 있도록 지정
-        binding.vpHomePanel.orientation = ViewPager2.ORIENTATION_HORIZONTAL
-
-
-        val bannerAdapter = HomePanelVPAdapter(this)
-        bannerAdapter.addItem(HomeBannerFragment(R.drawable.img_home_viewpager_exp))
-        bannerAdapter.addItem(HomeBannerFragment(R.drawable.img_home_viewpager_exp2))
-        bannerAdapter.addItem(HomeBannerFragment(R.drawable.img_home_viewpager_exp))
-        bannerAdapter.addItem(HomeBannerFragment(R.drawable.img_home_viewpager_exp2))
-        binding.vpHomeBanner.adapter = bannerAdapter
-        binding.vpHomeBanner.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        initBanner()
+        initPanel()
 
         binding.ivAlbumImage1.setOnClickListener {
             // replace ( 바꿀 화면 id, 반영할 프래그먼트)
-            var bundle = Bundle()
+            val bundle = Bundle()
             bundle.putString("title",binding.tvAlbumTitle1.text.toString())
             bundle.putString("singer", binding.tvAlbumSinger1.text.toString())
             val fragment = AlbumFragment().apply {
@@ -58,6 +43,32 @@ class HomeFragment : Fragment() {
         }
 
         return  binding.root
+    }
+
+    private fun initPanel() {
+        val panelAdapter = HomePanelVPAdapter(this)
+        val bundle = Bundle()
+        panelAdapter.addItem(HomePanelFragment(R.drawable.img_first_album_default))
+        panelAdapter.addItem(HomePanelFragment(R.drawable.ic_launcher_background))
+        panelAdapter.addItem(HomePanelFragment(R.drawable.img_first_album_default))
+        panelAdapter.addItem(HomePanelFragment(R.drawable.ic_launcher_background))
+        binding.vpHomePanel.adapter = panelAdapter
+        //뷰페이져가 좌우로 스크롤 될 수 있도록 지정
+        binding.vpHomePanel.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
+        // indicator 설정
+        binding.panelIndicator.setViewPager(binding.vpHomePanel)
+
+    }
+
+    private fun initBanner() {
+        val bannerAdapter = HomePanelVPAdapter(this)
+        bannerAdapter.addItem(HomeBannerFragment(R.drawable.img_home_viewpager_exp))
+        bannerAdapter.addItem(HomeBannerFragment(R.drawable.img_home_viewpager_exp2))
+        bannerAdapter.addItem(HomeBannerFragment(R.drawable.img_home_viewpager_exp))
+        bannerAdapter.addItem(HomeBannerFragment(R.drawable.img_home_viewpager_exp2))
+        binding.vpHomeBanner.adapter = bannerAdapter
+        binding.vpHomeBanner.orientation = ViewPager2.ORIENTATION_HORIZONTAL
     }
 
     override fun onDestroyView() {
--- mission/Flo/app/src/main/java/com/example/flo/ui/home/HomePanelFragment.kt
+++ mission/Flo/app/src/main/java/com/example/flo/ui/home/HomePanelFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.flo.databinding.ItemVpHomePanelBinding
 
-class HomePanelFragment: Fragment() {
+class HomePanelFragment(private val imgRes : Int): Fragment() {
     lateinit var binding : ItemVpHomePanelBinding
 
     override fun onCreateView(
@@ -16,7 +18,7 @@ class HomePanelFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = ItemVpHomePanelBinding.inflate(inflater,container,false)
-
+        binding.root.background = ContextCompat.getDrawable(requireContext(),imgRes)
         return binding.root
 
     }
