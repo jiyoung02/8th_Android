b09e00f34a2417ae1a26358379df83f43d3f7202
iwannacat <ju0808lie@naver.com>
2025-05-07 23:44:59 +0900

    mission: flo 보관함 화면 구현

--- mission/Flo/app/src/main/java/com/example/flo/data/Song.kt
+++ mission/Flo/app/src/main/java/com/example/flo/data/Song.kt
@@ -11,5 +11,6 @@ data class Song(
     var second : Int = 0,
     var playTime : Int = 60,
     var isPlaying : Boolean = false,
-    var music : String = ""
+    var music : String = "",
+    var coverImg : Int? = null
 ) : Parcelable
